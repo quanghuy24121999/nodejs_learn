0d278bf04c3c7613690e95e9d2c795001a9a098a
quanghuy24121999 <quanghuy24121999@gmail.com>
2021-05-05 22:46:57 +0700

    add course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
+    name: { type: String, required: true },
     description: { type: String, maxLength: 255 },
-    image: { type: String, maxLength: 255 },
-    createAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
+    image: { type: String },
+    videoId: { type: String },
+    level: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
